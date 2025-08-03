e7bc9d51b5d7b1e060c5f9c59e1db1e7f6ebb0f3
Mark Daniel Garnica <markdgarnica@gmail.com>
2025-08-04 07:59:16 +0800

    in progress about me section

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,4 +3,38 @@ import IntroSection from '@/components/sections/IntroSection.vue'
 </script>
 <template>
   <IntroSection />
+  <div class="max-w-6xl mx-auto py-8">
+    <div class="pl-5 mb-9">
+      <h1 class="uppercase">Me</h1>
+      <div class="w-11 h-[1px] bg-light"></div>
+    </div>
+    <div class="w-full flex flex-row gap-6">
+      <!-- Slide Pagination -->
+      <div></div>
+      <div class="flex flex-row">
+        <div class="w-1/3">
+          <p class="leading-6">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Amet consectetur adipiscing elit
+            quisque faucibus ex sapien. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+            Vitae pellentesque sem placerat in id cursus mi.
+          </p>
+          <p class="leading-6">
+            Lorem ipsum dolor sit amet consectetur adipiscing elit. Amet consectetur adipiscing elit
+            quisque faucibus ex sapien. Quisque faucibus ex sapien vitae pellentesque sem placerat.
+            Vitae pellentesque sem placerat in id cursus mi.
+          </p>
+        </div>
+        <div class="w-2/3">
+          <div class="w-full bg-gray bg-opacity-[5%]">
+            <p class="text-gold">As a Developer.....</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipiscing elit. Amet consectetur adipiscing
+              elit quisque faucibus ex sapien. Quisque faucibus ex sapien vitae pellentesque sem
+              placerat. Vitae pellentesque sem placerat in id cursus mi.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
